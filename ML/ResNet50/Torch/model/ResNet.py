21a49c65fc7037fd3284e157d895ec869aeb61ee
BigBalloon8 <raecd123@gmail.com>
2023-11-13 14:33:34 +0000

    first iteration of resnet and deepcam

--- ML/ResNet50/Torch/model/ResNet.py
+++ ML/ResNet50/Torch/model/ResNet.py
@@ -2,63 +2,72 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ResNetBottleneck(nn.Module):
+class Bottleneck(nn.Module):
     expansion = 4
-    def __init__(self, in_channels, out_channels, stride=1):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
-        if stride != 1 or in_channels != out_channels*self.expansion:
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels*self.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels*self.expansion)
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
             )
-        else:
-            self.shortcut = []
-    
+
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = self.bn3(self.conv3(x))
-        x = x + self.shortcut(x)
-        return F.relu(x)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
 
 class ResNet50(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes=1000):
         super().__init__()
-        num_blocks = [3,4,6,3]
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        num_blocks = [3, 4, 6, 3]
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(Bottleneck, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(Bottleneck, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(Bottleneck, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(Bottleneck, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*Bottleneck.expansion, num_classes)
 
-        self.layer1 = self._make_block(64, num_blocks[0], 1)
-        self.layer2 = self._make_block(128, num_blocks[1], 2)
-        self.layer3 = self._make_block(256, num_blocks[2], 2)
-        self.layer4 = self._make_block(512, num_blocks[3], 2)
-
-        self.linear = nn.Linear(512*ResNetBottleneck.expansion, num_classes)
-        
-    
-    def _make_block(self, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(ResNetBottleneck(self.in_channels, planes, stride))
-            self.in_channels = planes * ResNetBottleneck.expansion
-            return nn.Sequential(*layers)
-    
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = F.avg_pool2d(x, 4)
-        x = x.view(x.size(0), -1)
-        x = self.linear(x)
-        return x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.max_pool2d(out, (3,3), (2,2), (1,1))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.adaptive_avg_pool2d(out, (1,1))
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+if __name__ == "__main__":
+    model = ResNet50(1000)
+    img = torch.randn(2, 3, 244, 244)
+    model(img)
--- ML_HPC/CosmoFlow/Torch/data/TF_record_loader.py
+++ ML_HPC/CosmoFlow/Torch/data/TF_record_loader.py
@@ -5,3 +5,29 @@ def get_datapipe(path):
     datapipe1 = torchdata.datapipes.iter.FileLister(path, "*.tfrecord")
     datapipe2 = torchdata.datapipes.iter.FileOpener(datapipe1, mode="b")
     return datapipe2.load_from_tfrecord()
+from tfrecord.torch.dataset import TFRecordDataset
+import os
+from torchdata.datapipes.iter import FileLister, FileOpener, TFRecordLoader
+
+if __name__ == "__main__":
+    """
+    path = "/work/z19/shared/mlperf-hpc/cosmoflow/full/cosmoUniverse_2019_05_4parE_tf_v2/train/"
+    file = os.listdir(path)[0]
+    full_path = os.path.join(path, file)
+    print(full_path)
+    desc = {"x": "str", "y": "float"}
+    dataset = TFRecordDataset(full_path, None, desc)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=32)
+
+    data = next(iter(loader))
+    print(data)
+    """
+    path = "/work/z19/shared/mlperf-hpc/cosmoflow/full/cosmoUniverse_2019_05_4parE_tf_v2/train/"
+    datapipe1 = FileLister(path, "22098324_univ_ics_2019-03_a1099062_*.tfrecord")
+    print(len(list(datapipe1)))
+    datapipe2 = FileOpener(datapipe1, mode="b")
+    tfrecord_loader_dp = TFRecordLoader(datapipe2)
+    print(dir(tfrecord_loader_dp))
+    for i in list(tfrecord_loader_dp):
+        print(i)
+        exit()
