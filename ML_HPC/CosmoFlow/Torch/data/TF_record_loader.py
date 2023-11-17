153e0555fb9d1096e9f18c376f8e82e9837c7cb3
BigBalloon8 <raecd123@gmail.com>
2023-11-17 11:13:58 +0000

    fixed tfrecord to torch

--- ML_HPC/CosmoFlow/Torch/data/TF_record_loader.py
+++ ML_HPC/CosmoFlow/Torch/data/TF_record_loader.py
@@ -1,33 +1,74 @@
-import torch
-import torchdata
-
-def get_datapipe(path):
-    datapipe1 = torchdata.datapipes.iter.FileLister(path, "*.tfrecord")
-    datapipe2 = torchdata.datapipes.iter.FileOpener(datapipe1, mode="b")
-    return datapipe2.load_from_tfrecord()
-from tfrecord.torch.dataset import TFRecordDataset
 import os
-from torchdata.datapipes.iter import FileLister, FileOpener, TFRecordLoader
+
+import torch
+from torch.utils.data import Dataset, DataLoader, DistributedSampler, RandomSampler
+from tfrecord.reader import tfrecord_loader
+
+from ML.gc import GlobalContext
+
+gc = GlobalContext()
+
+class CosmoDataset(Dataset):
+    def __init__(self, dataset_path, train=True):
+        if train:
+            size = gc["data"]["n_train"]
+        else:
+            size = gc["data"]["n_eval"]
+        self.files = os.listdir(dataset_path)[:size]
+        self.root - dataset_path
+        self.length = len(self.files)
+    
+    def len(self):
+        return self.length
+    
+    def __getitem__(self, index):
+        # Theres only one sample per file
+        data = next(tfrecord_loader(os.path.join(self.root, self.files[index]),
+                                    description={"x": "byte", "y":"float"},
+                                    compression_type=gc["data"]["compression"]
+                                    ))
+        x = torch.frombuffer(data["x"], dtype=torch.int16)
+        x = torch.reshape(x, [128,128,128,4]).to(torch.float32)
+        y = torch.tensor(data["y"], dtype="float32")
+
+        if gc["data"]["apply_log"]:
+            x = torch.log(x+1)
+        else:
+            x /= (torch.sum(x)/torch.prod(torch.tensor(x.shape)))
+        
+        return x, y
+    
+def get_train_dataloader():
+    path = os.pat.join(gc["data"]["data_dir"], "train/")
+    data = CosmoDataset(path, train=True)
+    if gc.world_size > 1:
+        sampler = DistributedSampler(data, gc.world_size, gc.rank, shuffle=gc["data"]["shuffle"], drop_last=True)
+    else:
+        sampler = RandomSampler(data)
+    local_bs = gc["data"]["global_batch_size"] // gc.world_size
+    return DataLoader(data, 
+                      sampler=sampler, 
+                      batch_size=local_bs, 
+                      shuffle=gc["data"]["shuffle"],
+                      drop_last=True,
+                      pin_memory=True if gc.device != "cpu" else False
+                      )
+
+def get_val_dataloader():
+    path = os.pat.join(gc["data"]["data_dir"], "validation/")
+    data = CosmoDataset(path, train=False)
+    if gc.world_size > 1:
+        sampler = DistributedSampler(data, gc.world_size, gc.rank, shuffle=gc["data"]["shuffle"], drop_last=True)
+    else:
+        sampler = RandomSampler(data)
+    local_bs = gc["data"]["global_batch_size"] // gc.world_size
+    return DataLoader(data, 
+                      sampler=sampler, 
+                      batch_size=local_bs, 
+                      shuffle=gc["data"]["shuffle"],
+                      drop_last=True,
+                      pin_memory=True if gc.device != "cpu" else False)
+
 
 if __name__ == "__main__":
-    """
-    path = "/work/z19/shared/mlperf-hpc/cosmoflow/full/cosmoUniverse_2019_05_4parE_tf_v2/train/"
-    file = os.listdir(path)[0]
-    full_path = os.path.join(path, file)
-    print(full_path)
-    desc = {"x": "str", "y": "float"}
-    dataset = TFRecordDataset(full_path, None, desc)
-    loader = torch.utils.data.DataLoader(dataset, batch_size=32)
-
-    data = next(iter(loader))
-    print(data)
-    """
-    path = "/work/z19/shared/mlperf-hpc/cosmoflow/full/cosmoUniverse_2019_05_4parE_tf_v2/train/"
-    datapipe1 = FileLister(path, "22098324_univ_ics_2019-03_a1099062_*.tfrecord")
-    print(len(list(datapipe1)))
-    datapipe2 = FileOpener(datapipe1, mode="b")
-    tfrecord_loader_dp = TFRecordLoader(datapipe2)
-    print(dir(tfrecord_loader_dp))
-    for i in list(tfrecord_loader_dp):
-        print(i)
-        exit()
+    ...
