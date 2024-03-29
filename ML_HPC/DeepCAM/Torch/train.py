928bd6be9094dc7b7217229c9612ee979f49a9d2
BigBalloon8 <raecd123@gmail.com>
2024-03-29 10:54:17 +0000

    fix gc import bug

--- ML_HPC/DeepCAM/Torch/train.py
+++ ML_HPC/DeepCAM/Torch/train.py
@@ -1,6 +1,15 @@
 import os
 import sys
-path_root = "/".join(sys.argv[0].split("/")[:-4])
+import inspect
+def get_current_file():
+    # Get the current frame
+    current_frame = inspect.currentframe()
+    # Get the outer frame (one level up), to get the caller's file name, not this utility's filename
+    caller_frame = inspect.getouterframes(current_frame)[1]
+    # Extract the file path
+    file_path = caller_frame.filename
+    return file_path
+path_root = "/".join(get_current_file().split("/")[:-4])
 sys.path.append(str(path_root))
 import time
 import warnings
